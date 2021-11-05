7d6bf6574d603232673b0bff4d95f898ebba0c3c
LutencuOctavian <octavv2001@yahoo.com>
2021-11-05 20:31:00 +0200

    spring boot app whit dataBase connection SQL

---
+++ demo-BookShop/src/test/java/com/example/demoBookShop/DemoBookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoBookShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
